977e72186ffbc91c377e439ba4455117750d2544
thaison-NewBie <lethaison199@gmail.com>
2025-11-06 21:35:14 +0700

    Create server and connect with mongoose

---
+++ server/server.js
@@ -0,0 +1,15 @@
+import express, { request } from "express"
+import "dotenv/config";
+import cors from "cors";
+import connectDB from "./configs/db.js";
+
+connectDB()
+
+const app = express()
+app.use(cors()) // Enable Cross-Origin Resource Sharing (Allow Front-end connect to Back-end)
+
+app.get('/', (req, res)=> res.send("API is working"))
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, ()=> console.log(`Server running on port ${PORT}`));
