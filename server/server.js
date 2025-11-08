a7424ab0553ced31e147ceb3f1733429d9e29f36
thaison-NewBie <lethaison199@gmail.com>
2025-11-08 14:51:01 +0700

    commit

--- server/server.js
+++ server/server.js
@@ -2,11 +2,21 @@ import express, { request } from "express"
 import "dotenv/config";
 import cors from "cors";
 import connectDB from "./configs/db.js";
+import { clerkMiddleware } from '@clerk/express'
+import clerkWebhooks from "./controllers/clerkWebhooks.js";
 
 connectDB()
 
 const app = express()
 app.use(cors()) // Enable Cross-Origin Resource Sharing (Allow Front-end connect to Back-end)
+
+
+// Middleware
+app.use(express.json())
+app.use(clerkMiddleware())
+
+// API to listen Clerk Webhooks
+app.use("/api/clerk", clerkWebhooks);
 
 app.get('/', (req, res)=> res.send("API is working"))
 
